0302a95057c1c1de81c4942a017e054d0fafd231
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-10-11 23:58:22 +0100

    first commit

---
+++ BlazorApp1/Models/Tickets.cs
@@ -0,0 +1,22 @@
+﻿public class Tickets
+{
+
+    public string? ticket_id { get; set; }
+    public string? ticket_subject { get; set; }
+    public string? ticket_description { get; set; }
+    public string? created_by { get; set; }
+    public DateTime created_at { get; set; }
+    public string? priority { get; set; }
+    public string? status { get; set; }
+
+    public int DaysOld(DateTime d)
+    {
+        DateTime d1 = DateTime.Now;
+
+
+        TimeSpan t = d1 - d;
+        double NrOfDays = t.TotalDays;
+        return (int)NrOfDays;
+    }
+
+}
